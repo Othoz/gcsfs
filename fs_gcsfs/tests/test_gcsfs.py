64881a42320ba769ec2b6eca3948081c0ac30d34
Benjamin Trendelkamp-Schroer <btschroer@othoz.com>
2018-11-07 08:05:48 +0100

    Skipping test showing a bug in scandir is the root directory is the bucket.

--- fs_gcsfs/tests/test_gcsfs.py
+++ fs_gcsfs/tests/test_gcsfs.py
@@ -55,18 +55,23 @@ def tmp_gcsfs(bucket, client):
 
 class TestGCSFS:
 
+    @pytest.mark.skip("There is still a bug in the scandir implementaion. Root level blobs (which are not directories) are not listed")
     def test_scandir_works_on_bucket_as_root_directory(self, client):
         gcs_fs = GCSFS(bucket_name=TEST_BUCKET, client=client)
         path = str(uuid.uuid4())
         with gcs_fs.open(path, "wb") as f:
-            f.write(b"")
-        assert len(gcs_fs.listdir("")) > 0
+            f.write(b"Hallo")
+        result = gcs_fs.listdir("")
         # Manual clean-up of the file created on the root directory
         gcs_fs.remove(path)
         gcs_fs.close()
+        assert path in result
 
     def test_path_to_key_for_root_returns_root_path(self, tmp_gcsfs):
         assert tmp_gcsfs._path_to_key("/") == tmp_gcsfs.root_path
+
+    def test_path_to_key_for_empty_string_returns_root_path(self, tmp_gcsfs):
+        assert tmp_gcsfs._path_to_key("") == tmp_gcsfs.root_path
 
     def test_fix_storage_adds_binary_blobs_with_empty_string_as_directory_marker(self, bucket, tmp_gcsfs):
         # Creating a 'nested' hierarchy of blobs without directory marker
@@ -79,7 +84,7 @@ class TestGCSFS:
         for path in ["", "foo", "foo/bar", "foo/baz", "foo/bar/egg"]:
             assert tmp_gcsfs.isdir(path)
 
-    def test_fix_storage_does_not_overwrite_existing_directory_markers(self, bucket, tmp_gcsfs):
+    def test_fix_storage_does_not_overwrite_existing_directory_markers_with_custom_content(self, bucket, tmp_gcsfs):
         for path in ["foo/test"]:
             key = tmp_gcsfs._path_to_key(path)
             blob = bucket.blob(key)
