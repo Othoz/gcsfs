9773509bcc2fe62ac6de18d275537996af437dcf
Philipp Wiesner <wiesner@othoz.com>
2018-11-07 11:49:49 +0100

    another try on travis

--- fs_gcsfs/__version__.py
+++ fs_gcsfs/__version__.py
@@ -1,7 +1,7 @@
-import subprocess
+import subprocess  # nosec
 
 try:
-    __version__ = subprocess.check_output(["git", "describe"]).decode().strip()
+    __version__ = subprocess.check_output(["git", "describe"]).decode().strip()  # nosec
 except subprocess.CalledProcessError:
     print("Please only import {} from within a git repository with at least one tagged commit".format(__file__))
     __version__ = "unknown"
