cb5c8760bfc9e901ddaa8c8f84729f8528e4e4e7
Mathias Eitz <eitz@othoz.com>
2020-05-18 16:42:25 +0200

    externalize script to have more control

---
+++ run-tests.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -ev
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo PR is an internal PR coming from "$TRAVIS_REPO_SLUG".
+  openssl aes-256-cbc -K $encrypted_b8e3bf9ce0e9_key -iv $encrypted_b8e3bf9ce0e9_iv -in .travis/service-account-key.json.enc -out $HOME/service-account-key.json -d
+  export GOOGLE_APPLICATION_CREDENTIALS=$HOME/service-account-key.json
+  export TEST_BUCKET=othoz-gcsfs-tests
+  pytest --cov=fs_gcsfs --cov-branch --cov-report=xml --numprocesses=16
+else
+  echo PR coming from "$TRAVIS_REPO_SLUG". Not running test suite as it requires credentials that are not available when running a PR from a forked repo.
+fi
