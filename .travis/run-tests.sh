331120ea0dab21aff3b52a808e747801ea04d75d
Mathias <eitz@othoz.com>
2020-05-20 08:22:14 +0200

    Update credentials for GCP service account (#33)

--- .travis/run-tests.sh
+++ .travis/run-tests.sh
@@ -5,7 +5,7 @@ echo TRAVIS_REPO_SLUG is set to "$TRAVIS_REPO_SLUG"
 echo TRAVIS_PULL_REQUEST is set to "$TRAVIS_PULL_REQUEST"
 if [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
   echo PR is an internal PR coming from "$TRAVIS_REPO_SLUG".
-  openssl aes-256-cbc -K "$encrypted_335424c81256_key" -iv "$encrypted_335424c81256_iv" -in .travis/service-account-key.json.enc -out "$HOME"/service-account-key.json -d
+  openssl aes-256-cbc -K "$encrypted_c49b49c0ca67_key" -iv "$encrypted_c49b49c0ca67_iv" -in .travis/service-account-key.json.enc -out "$HOME"/service-account-key.json -d
   export GOOGLE_APPLICATION_CREDENTIALS=$HOME/service-account-key.json
   export TEST_BUCKET=othoz-gcsfs-tests
   pytest --cov=fs_gcsfs --cov-branch --cov-report=xml --numprocesses=16
